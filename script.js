434632ea681ea0f9e37a91458ab77b7d82c3cb6a
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2025-02-20 20:39:55 +0700

    proxy

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const targetUrl = "https://t.if.co.id/json/Fauzi.json";
 
 // Fetch JSON data from the provided URL
-getJSON(proxyUrl + targetUrl, "null", "null", responseFunction);
+getJSON(targetUrl, "null", "null", responseFunction);
 
 // Function to handle the response and render the content
 function responseFunction(response) {
